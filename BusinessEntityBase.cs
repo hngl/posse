36220571e2e217874c00a253ae3c8f6d694faf17
Jurriaan <15790266+hngl@users.noreply.github.com>
2017-08-05 22:33:36 +0200

    Editable list [RUNS on Android and iOS]

---
+++ BusinessEntityBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SQLite;
+
+namespace Posse
+{
+    // The base class implements the interface and adds the SQLite-NET 
+    // attributes to mark it as an auto-incrementing primary key. Any class in 
+    // the Business Layer that implements this base class can then be persisted 
+    // in the Data Layer
+    public abstract class BusinessEntityBase : IBusinessEntity
+    {
+
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        // No constructor, because empty
+    }
+}
